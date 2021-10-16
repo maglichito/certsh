d1ff0dc13a600eb4c3e4ab1df48d7438c4d010f9
Dzenan Becirovic <dzenanbecirovicc@gmail.com>
2021-10-16 02:09:39 +0200

    test

---
+++ certsh.py
@@ -0,0 +1,41 @@
+import chromedriver_autoinstaller
+import getopt, sys
+import argparse
+import colorama
+from colorama import Fore, Back, Style
+from selenium import webdriver
+
+
+# Checking if argument -t/--target is passed
+parser = argparse.ArgumentParser(prog='base_maker',
+                                 description=f'{Fore.LIGHTRED_EX}CERTSH - Automated procces of finding subdomains on crt.sh',
+                                 epilog=f'{Fore.YELLOW}Example: python3 certsh.py -t www.example.com',
+                                 formatter_class=argparse.RawDescriptionHelpFormatter)
+parser.add_argument('-t','--target',required=True)
+args = parser.parse_args()
+
+#Target
+target = args.target
+chromedriver_autoinstaller.install()
+
+# Getting Chrome as a browser
+option = webdriver.ChromeOptions()
+option.add_argument('headless')
+browser = webdriver.Chrome(options=option)
+
+# URL
+u = 'https://crt.sh/'
+
+# Opening browser and getting to url
+browser.get(u)
+
+argv = sys.argv[1:]
+def search():
+    search_box = browser.find_element_by_xpath("//form/input[@type='text']")
+    search_box.send_keys(target)
+    search_box.submit()
+    xpath = "/html/body/table[2]/tbody/tr/td/table/tbody/tr/td[5]"
+    rows = browser.find_elements_by_xpath(xpath)
+    for row in rows[1:]:
+        print(row.text)
+search()
